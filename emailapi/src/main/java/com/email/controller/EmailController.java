c6d483036bb7159a8ca0d5ba296b91eed4313252
Shaker <aishaker129@gmail.com>
2025-05-05 02:40:14 +0600

    add email web api backend

--- emailapi/src/main/java/com/email/controller/EmailController.java
+++ emailapi/src/main/java/com/email/controller/EmailController.java
@@ -1,13 +1,36 @@
 package com.email.controller;
 
+import com.email.model.EmailRequest;
+import com.email.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
 
+    @Autowired
+    EmailService emailService;
+
     @GetMapping("/home")
     public String welcome(){
         return "Hello this is just for testing";
+    }
+
+    @PostMapping("/sendemail")
+    public ResponseEntity<?> sendMessage(@RequestBody EmailRequest request){
+        System.out.println(request);
+        boolean result =  this.emailService.sendEmail(request.getSubject(),request.getMessage(),request.getTo());
+        if(result){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Email send successfully");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email not send....");
+        }
+
     }
 }
--- emailapi/src/main/java/com/email/service/EmailService.java
+++ emailapi/src/main/java/com/email/service/EmailService.java
@@ -2,27 +2,78 @@ package com.email.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+
 
 @Service
 public class EmailService {
 
-    public void sendEmail(String subject, String message, String to){
-        // rest of the code...
+    public boolean sendEmail(String subject, String message, String to){
 
+        boolean f = false;
+
+        String from = "helloaishaker@gmail.com";
         // variable for gmail
         String host = "smtp.gmail.com";
 
         // get the system properties
-        Properties properties = System.getProperties();
-        System.out.println("PROPERTIES "+properties);
+        Properties properties =  System.getProperties();
+        System.out.println("PROPERTIES :" + properties);
 
-        // setting import information to properties object
+        // setting important information to properties
 
         // host set
-        properties.put("mail.smtp.host",host);
-        properties.put("mail.smtp.port","465");
-        properties.put("mail.smtp.ssl.enable","true");
-        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+
+        // Step 1: to get the session object
+        Session session = Session.getInstance(properties, new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                // TODO Auto-generated method stub
+                return new PasswordAuthentication("helloaishaker@gmail.com", "********");
+            }
+
+        });
+
+        session.setDebug(true);
+
+        // Step 2: compose the message(text,multi media)
+
+        MimeMessage m = new MimeMessage(session);
+
+        try {
+            // from email
+            m.setFrom(from);
+
+            // adding recipent to message
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // adding subject to message
+            m.setSubject(subject);
+
+            // adding test to message
+            m.setText(message);
+
+            // send message
+
+            // Step 3: send the message using Transport class
+            Transport.send(m);
+
+            System.out.println("Sent successfully....");
+
+            f = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return f;
     }
 }
